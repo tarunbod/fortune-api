6ef6ea89d251b3b4e3180b13270b878bda7e4071
Tarun Boddupalli <tarunbod@gmail.com>
2018-07-15 16:26:09 +0530

    added cors support

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 var router = require('express').Router();
 var fortunes = require('./fortunes');
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 router.get('/', function(req, res) {
     if (req.query['id']) {
         // res.json(fortunes.byId(req.query['id']));
